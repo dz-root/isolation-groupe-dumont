4186d733d915a7a89a1528f0301d5d0b418c5930
Kingweb <dz-root@hotmail.fr>
2025-11-20 15:32:31 +0100

    Update call btn

--- src/lib/CallButton.svelte
+++ src/lib/CallButton.svelte
@@ -4,7 +4,7 @@
   
     onMount(() => {
       const handleScroll = () => {
-        visible = window.scrollY > 400;
+        visible = window.scrollY > 200;
       };
   
       window.addEventListener('scroll', handleScroll);
@@ -13,13 +13,9 @@
   </script>
   
   <!-- Bouton fixe, visible uniquement sur mobile -->
-  <a
-    href="tel:+33782376522"
-    class={`md:hidden fixed bottom-4 right-4 z-50 transition-all duration-300
-            ${visible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-5 pointer-events-none'}`}
-  >
-    <button class="bg-green-600 text-white font-semibold px-5 py-3 rounded-full shadow-lg hover:bg-green-700 active:scale-95">
-        <svg class="w-5" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+  <a href="tel:+33782376522" class={`md:hidden fixed bottom-4 right-4 z-50 transition-all duration-300 ${visible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-5 pointer-events-none'}`}>
+    <button class="flex items-center gap-2 justify-center bg-gradient-to-l from-green-700 to-green-500 text-white font-semibold px-5 py-3 rounded-full shadow-lg hover:bg-green-700 active:scale-95">
+        <svg class="w-5" data-slot="icon" fill="currentColor" stroke-width="1.5" stroke="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
             <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"></path>
         </svg>
         Nous appeler
